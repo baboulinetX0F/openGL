968144bae648bf6965023a95f3fc45ac116ef5ab
baboulinet <vergilotaku@live.fr>
2016-09-16 16:42:57 +0200

    added EBO / draw a rectangle

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -52,9 +52,15 @@ int main(int argc, int argv)
 
 	// Test Rendering Data
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,  // Top Right
+		0.5f, -0.5f, 0.0f,  // Bottom Right
+		-0.5f, -0.5f, 0.0f,  // Bottom Left
+		-0.5f,  0.5f, 0.0f   // Top Left 
+	};
+
+	GLuint indices[] = {  
+		0, 1, 3,   
+		1, 2, 3    
 	};
 
 	GLuint VBO;
@@ -62,6 +68,9 @@ int main(int argc, int argv)
 
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
 
 	// Vertex Shader
 	std::ifstream ifs("default.vshader");
@@ -120,12 +129,14 @@ int main(int argc, int argv)
 	glBindVertexArray(VAO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 	glBindVertexArray(0);
 
 	glUseProgram(shaderProgram);
-
+	
 	// Main Loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -136,7 +147,7 @@ int main(int argc, int argv)
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
