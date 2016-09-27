2215561b435bb471ad3d1d02c7b3423f53eb1f81
baboulinet <vergilotaku@live.fr>
2016-09-28 01:22:37 +0200

    directionnal lighting

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -148,6 +148,19 @@ int main(int argc, int argv)
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f,  0.0f
 	};	
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f, 3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f, 2.0f, -2.5f),
+		glm::vec3(1.5f, 0.2f, -1.5f),
+		glm::vec3(-1.3f, 1.0f, -1.5f)
+	};
+
 	GLuint VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
@@ -174,15 +187,15 @@ int main(int argc, int argv)
 
 	_cam = new Camera();
 
-	Shader* shad = new Shader("shaders/materialLightSpecularMap.vert", "shaders/materialLSEMap.frag");
+	Shader* shad = new Shader("shaders/materialLightSpecularMap.vert", "shaders/light_casters/directionalLighting.frag");
 	Shader* lampShader = new Shader("shaders/lamp.vert", "shaders/lamp.frag");
 	
 	GLuint woodTex;
 	GLuint specularWoodTex;
-	GLuint emissionMap;
+	//GLuint emissionMap;
 	glGenTextures(1, &woodTex);
 	glGenTextures(1, &specularWoodTex);
-	glGenTextures(1, &emissionMap);
+	//glGenTextures(1, &emissionMap);
 
 	int texWidth, texHeight;
 	unsigned char* image;
@@ -210,7 +223,7 @@ int main(int argc, int argv)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 
 	// Emission Map Texture
-	glBindTexture(GL_TEXTURE_2D, emissionMap);
+	/* glBindTexture(GL_TEXTURE_2D, emissionMap);
 	image = SOIL_load_image("textures/matrix_emission.jpg", &texWidth, &texHeight, 0, SOIL_LOAD_RGB);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
@@ -218,7 +231,7 @@ int main(int argc, int argv)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST); */
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	// Uniform Lighting Shader Location
@@ -251,13 +264,16 @@ int main(int argc, int argv)
 	{
 		glfwPollEvents();
 				
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shad->Use();
 
-		GLint lightPosLoc = glGetUniformLocation(shad->_program, "light.position");
-		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+		/*GLint lightPosLoc = glGetUniformLocation(shad->_program, "light.position");
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);*/
+
+		GLint lightDirPos = glGetUniformLocation(shad->_program, "light.direction");
+		glUniform3f(lightDirPos, -0.2f, -1.0f, -0.3f);
 
 		glm::mat4 view, projection;
 
@@ -278,14 +294,21 @@ int main(int argc, int argv)
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, specularWoodTex);
 
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, emissionMap);
+		/*glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, emissionMap);*/
 
-		glBindVertexArray(VAO);	
-		glm::mat4 model;			
-		model = glm::translate(model, glm::vec3(0.0f,0.0f,2.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);		
+		glm::mat4 model;
+
+		glBindVertexArray(VAO);
+		for (int i = 0; i < 10; i++)
+		{			
+			model = glm::mat4();
+			model = glm::translate(model, cubePositions[i]);
+			GLfloat angle = 20 * i;
+			model = glm::rotate(model,angle, glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		glBindVertexArray(0);
 
 		lampShader->Use();		
@@ -296,6 +319,7 @@ int main(int argc, int argv)
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(lightVAO);
+		model = glm::mat4();
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
